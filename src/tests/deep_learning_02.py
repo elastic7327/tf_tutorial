472408f930e7c0be5077c6d84f4af986227e9aaa
dan <elastic7327@gmail.com>
2018-04-01 21:54:57 +0900

    Update

--- src/tests/deep_learning_02.py
+++ src/tests/deep_learning_02.py
@@ -31,3 +31,43 @@ class TestBasicNNetwork(TensorFlowTestBase):
 
             print(y_out)
 
+    def test_what_is_rank(self):
+        with self.test_session() as sess:
+            scalar = tf.constant(100)
+            vector = tf.constant([1, 2, 3, 4, 5])
+            matrix = tf.constant([[1 ,2, 3], [4 ,5 ,6]])
+            cube_matrix = tf.constant([[[1], [2], [3]], [[4], [5], [6]]])
+
+            print(scalar.get_shape())
+            print(vector.get_shape())
+            print(matrix.get_shape())
+            print(cube_matrix.get_shape())
+            print(sess.run(cube_matrix))
+
+            tendor_1d = np.array([1, 2, 3, 4, 5, 6, 7])
+            tendor_1d = tf.constant(tendor_1d)
+            print(tendor_1d.get_shape())
+            print(sess.run(tendor_1d))
+
+    def test_convert_to_tensor_test(self):
+        with self.test_session() as sess:
+            tendor_3d = np.array([[[1, 2, 3], [4, 5, 6], [7, 8, 9]]])
+
+            tendor_3d = tf.convert_to_tensor(tendor_3d, dtype=tf.float64)
+
+            print(tendor_3d.get_shape())
+            print(sess.run(tendor_3d))
+
+    def test_update_value_test(self):
+        one = tf.constant(1)
+        value = tf.Variable(0, name="value")
+        new_value = tf.add(value, one)
+        update_value = tf.assign(value, new_value)
+        init_var = tf.global_variables_initializer()
+        with self.test_session() as sess:
+            sess.run(init_var)
+            print(sess.run(value))
+
+            for _ in range(10):
+                sess.run(update_value)
+                print(sess.run(value))
